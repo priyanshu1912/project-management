f5035b2a47e87acbbe2e2262f7ce6df80dc729e8
balramyadav00 <yadavb957@gmail.com>
2022-09-09 16:31:29 +0530

    responsive

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,15 +3,16 @@ import React from "react";
 function Modal({ openModal, setOpenModal, text, setText, cols, setCols }) {
   return (
     <div
-      class="relative z-10"
+      class="relative z-10 sm:mx-4"
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true"
+      
     >
       <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
       <div class="fixed inset-0 z-10 overflow-y-auto">
         <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+          <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:mx-4 sm:w-full sm:max-w-lg">
             <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
               <input
                 type="text"
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      'sm': '300px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'lg': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {},
   },
   plugins: [],
