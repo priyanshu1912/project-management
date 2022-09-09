fb6914bd6e13ff1a317f1cc28b49e8c01913d53e
balramyadav00 <yadavb957@gmail.com>
2022-09-09 17:00:43 +0530

    update responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,13 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     screens: {
-      'sm': '300px',
+      'sm': {'max': '720px'},
       // => @media (min-width: 640px) { ... }
 
-      'md': '1024px',
+      'md': {'max':'1024px'},
       // => @media (min-width: 1024px) { ... }
 
-      'lg': '1280px',
+      'lg': {'max':'1280px'},
       // => @media (min-width: 1280px) { ... }
     },
     extend: {},
