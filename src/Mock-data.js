cb796173c331b42ebb20e95486a511eacb95a614
Priyanshu <priyanshubhardwaj19dec@gmail.com>
2022-09-16 23:04:26 +0530

    adding mock data

--- src/Mock-data.js
+++ src/Mock-data.js
@@ -74,13 +74,137 @@ export const completedTasks = [
   },
 ];
 
-
-
 export const users = [
   {
-    name: "testuser",
-    email: "testuser@gmail.com",
-    password: "testuser",
+    name: "Anand Bhagat",
+    email: "anand@gmail.com",
+    password: "anand",
+    projects: [
+      {
+        project_name: "redwing",
+        task_lists: [
+          {
+            task_list_name: "time tracking using ETA",
+            tasks: [
+              {
+                title:
+                  "Adding new command to flag a task for counting the defects of the developer and the team lead",
+                description:
+                  "Puneet when the task is marked as defective, then where to show this count?",
+                comments: [{}],
+                status: "uncompleted",
+              },
+              {
+                title: "Check daily slowdown of a user",
+                description:
+                  "we can track the slowdown by finding the difference between the two activities. If the difference is more than 3 hours then it will be calculated as a slowdown.",
+                comments: [{}],
+                status: "uncompleted",
+              },
+              {
+                title: "Past performance report changes",
+                description:
+                  "show bar chart only in the weekly tab and remove bar chart from the monthly tab",
+                comments: [{}],
+                status: "completed",
+              },
+              {
+                title:
+                  "Remove the full screen loader and show it in right top corner of the screen, so that when the api is loading, still we can perform action of checking performance etc",
+                description: "!add_budget 0.5",
+                comments: [{}],
+                status: "completed",
+              },
+            ],
+          },
+          {
+            task_list_name: "setup",
+            tasks: [
+              {
+                title:
+                  "Setup project locally, send me Github username, will invite to the repository.dding new command to flag a task for counting the defects of the developer and the team lead",
+                description: "invitation sent",
+                comments: [{}],
+                status: "uncompleted",
+              },
+            ],
+          },
+          {
+            task_list_name: "rapid estimation",
+            tasks: [
+              {
+                title: "API to login in",
+                description:
+                  "add a new table on the current database for the user list. This is not same as the current user list.",
+                comments: [{}],
+                status: "uncompleted",
+              },
+              {
+                title: "API Integartion to login in",
+                description: "What is the API for login integration?",
+                comments: [{}],
+                status: "uncompleted",
+              },
+              {
+                title: "API to create and update projects",
+                description:
+                  "These are not the projects currently in the project list check this UI, currently, it's using local storage and a flat-file. We need to convert it to the cloud. This is the flat file.",
+                comments: [{}],
+                status: "uncompleted",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Sakil Alam",
+    email: "sakil@gmail.com",
+    password: "sakil",
+    projects: [
+      {
+        project_name: "Tower App",
+        task_lists: [
+          {
+            task_list_name: "Nodles - Micro UI",
+            tasks: [
+              {
+                title: "Input widget: Measurement Location Selector",
+                description:
+                  "This widget helps select a location from a dropdown",
+                comments: [{}],
+                status: "uncompleted",
+              },
+              {
+                title: "Feat/QueryBuilder branch NPM issues",
+                description:
+                  "Need to wrap this up quickly. Can't we run react-querybuilder in version 17",
+                comments: [{}],
+                status: "completed",
+              },
+            ],
+          },
+          {
+            task_list_name: "Profile page review",
+            tasks: [
+              {
+                title: "Wrong date picker in profiler page",
+                description:
+                  "No the datepicker is same , there is diffrent custom styling applied there . Will figure it out",
+                comments: [{}],
+                status: "uncompleted",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Priyanshu Bhardwaj",
+    email: "priyanshu@gmail.com",
+    password: "priyanshu",
     projects: [
       {
         project_name: "redwing",
@@ -110,6 +234,154 @@ export const users = [
               {
                 title: "Past performance report changes",
                 description: "",
+                comments: [{}],
+                status: "completed",
+              },
+            ],
+          },
+          {
+            task_list_name: "setup",
+            tasks: [
+              {
+                title:
+                  "Adding new command to flag a task for counting the defects of the developer and the team lead",
+                description: "",
+                comments: [{}],
+                status: "uncompleted",
+              },
+              {
+                title: "Fixing the carry forward budget",
+                description: "",
+                comments: [{}],
+                status: "uncompleted",
+              },
+              {
+                title: "Check daily slowdown of a user",
+                description: "",
+                comments: [{}],
+                status: "uncompleted",
+              },
+              {
+                title: "Past performance report changes",
+                description: "",
+                comments: [{}],
+                status: "completed",
+              },
+            ],
+          },
+          {
+            task_list_name: "rapid estimation",
+            tasks: [
+              {
+                title:
+                  "Adding new command to flag a task for counting the defects of the developer and the team lead",
+                description: "",
+                comments: [{}],
+                status: "uncompleted",
+              },
+              {
+                title: "Fixing the carry forward budget",
+                description: "",
+                comments: [{}],
+                status: "uncompleted",
+              },
+              {
+                title: "Check daily slowdown of a user",
+                description: "",
+                comments: [{}],
+                status: "uncompleted",
+              },
+              {
+                title: "Past performance report changes",
+                description: "",
+                comments: [{}],
+                status: "completed",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        project_name: "GroTrack",
+        task_lists: [
+          {
+            task_list_name: "hydrafacial survey and consent",
+            tasks: [
+              {
+                title:
+                  "In the survey form, add number for each question, make the radio of text clickable so when the text is clicked the radio should get toogle and make the radio button bigger",
+                description: "yes, but add it only in last form",
+                comments: [{}],
+                status: "uncompleted",
+              },
+              {
+                title:
+                  "Please add red highlighted text in the hydrafacial survey form and logic of yellow highlighted text.",
+                description:
+                  "Yellow highlighted logic is clear, Red text means those are required?",
+                comments: [{}],
+                status: "completed",
+              },
+            ],
+          },
+          {
+            task_list_name: "glo track",
+            tasks: [
+              {
+                title:
+                  "please click all arrows next to the images. some of the arrows when clicked, the screen goes blank.",
+                description:
+                  "if the image is not available show msg like the below",
+                comments: [{}],
+                status: "uncompleted",
+              },
+              {
+                title: "Fixing the carry forward budget",
+                description: "",
+                comments: [{}],
+                status: "completed",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        project_name: "Intern Portal",
+        task_lists: [
+          {
+            task_list_name: "feedback cycle",
+            tasks: [
+              {
+                title: "Burndown - Feedback 5",
+                description:
+                  "Employer Dashboard: the links of my curriculums, my hired talents and search for a new talent should be in top navigation and not in the sidebar",
+                comments: [{}],
+                status: "uncompleted",
+              },
+              {
+                title:
+                  "Student Dashboard: On single course page, make the spacing consistent, check the attached video at timestamp 5:53",
+                description:
+                  "I have added an extra budget, so check all the other pages for the spacing issue and fixed it. Mentioned all the pages list in which you will be fixing the spacing issue. Will increase the budget accordingly.",
+                comments: [{}],
+                status: "completed",
+              },
+            ],
+          },
+          {
+            task_list_name: "Common screens",
+            tasks: [
+              {
+                title: "All available courses page",
+                description:
+                  "Template to embed list of courses. (Use left sections). The right section will be used to place the filter widget.",
+                comments: [{}],
+                status: "uncompleted",
+              },
+              {
+                title: "Course View Experience - View a single course",
+                description:
+                  "You can separate components in a separate file. But both will be shown on the same page",
                 comments: [{}],
                 status: "completed",
               },
