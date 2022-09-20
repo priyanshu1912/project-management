0a27d1f57d11fadd455431c3b0473b18bdb15121
Priyanshu <priyanshubhardwaj19dec@gmail.com>
2022-09-21 02:15:24 +0530

    changes

--- src/components/Taskpage/TaskPage.js
+++ src/components/Taskpage/TaskPage.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import { users } from "../../Mock-data.js";
+import { AiFillStar } from "react-icons/ai";
 
 function TaskPage() {
   const { state } = useLocation();
@@ -81,51 +82,115 @@ function TaskPage() {
             Comments ({task?.comments ? task?.comments?.length : 0})
           </div>
           <div>
-            <div className="flex items-center gap-2 mb-4">
+            <div className="flex gap-2 mb-6">
               <img
-                src="https://deejayfarm.com/wp-content/uploads/2019/10/Profile-pic.jpg"
+                src="https://pbs.twimg.com/profile_images/981311875643195393/dS0t6BQ8_400x400.jpg"
                 className="w-10 h-10 rounded-full object-cover"
               />
               <div>
-                <div className="font-semibold">
-                  Priyanshu Bhardwaj{" "}
-                  <span className="font-normal">42 seconds ago</span>
+                <div className="font-semibold">Priyanshu Bhardwaj </div>
+                <div className="font-normal">42 seconds ago</div>
+                <div className="flex items-center my-1">
+                  <AiFillStar color="#FFD700" size={18} />
+                  <AiFillStar color="#FFD700" size={18} />
+                  <AiFillStar color="#FFD700" size={18} />
                 </div>
-                <div>This is a test comment</div>
+                <div>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
+                  cum?
+                </div>
               </div>
             </div>
             {comments.length !== 0 &&
               comments.map((item) => {
                 return (
-                  <div className="flex items-center gap-2 mb-4">
+                  <div className="flex gap-2 mb-6">
                     <img
-                      src="https://deejayfarm.com/wp-content/uploads/2019/10/Profile-pic.jpg"
+                      src="https://pbs.twimg.com/profile_images/981311875643195393/dS0t6BQ8_400x400.jpg"
                       className="w-10 h-10 rounded-full object-cover"
                     />
                     <div>
-                      <div className="font-semibold">
-                        {item.userName}{" "}
-                        <span className="font-normal">{item.time}</span>
+                      <div className="font-semibold">{item.userName}</div>
+                      <div className="font-normal">{item.time}</div>
+                      <div className="flex items-center my-1">
+                        <AiFillStar color="#FFD700" size={18} />
+                        <AiFillStar color="#FFD700" size={18} />
+                        <AiFillStar color="#FFD700" size={18} />
                       </div>
                       <div>{item.comment}</div>
                     </div>
                   </div>
                 );
               })}
+            {comments.length !== 0 &&
+              comments.map((item, reviewIdx) => (
+                <div
+                  //key={review.id}
+                  className="flex text-sm text-gray-500 space-x-4"
+                >
+                  <div className="flex-none py-10">
+                    <img
+                      //src={review.avatarSrc}
+                      src="https://pbs.twimg.com/profile_images/981311875643195393/dS0t6BQ8_400x400.jpg"
+                      alt=""
+                      className="w-10 h-10 bg-gray-100 rounded-full"
+                    />
+                  </div>
+                  <div
+                    // className={classNames(
+                    //   reviewIdx === 0 ? "" : "border-t border-gray-200",
+                    //   "py-10"
+                    // )}
+                    className="py-10"
+                  >
+                    <h3 className="font-medium text-gray-900">
+                      {item.userName}
+                    </h3>
+                    <>
+                      {/* <time dateTime={review.datetime}>{review.date}</time> */}
+                      <div className="font-normal">{item.time}</div>
+                    </>
+
+                    <div className="flex items-center mt-4">
+                      {[0, 1, 2, 3, 4].map((rating) => (
+                        <AiFillStar
+                          color="yellow"
+                          key={rating}
+                          // className={classNames(
+                          //   review.rating > rating
+                          //     ? "text-yellow-400"
+                          //     : "text-gray-300",
+                          //   "h-5 w-5 flex-shrink-0"
+                          // )}
+                          aria-hidden="true"
+                        />
+                      ))}
+                    </div>
+                    {/* <p className="sr-only">{review.rating} out of 5 stars</p> */}
+
+                    <div
+                      className="mt-4 prose prose-sm max-w-none text-gray-500"
+                      dangerouslySetInnerHTML={{
+                        __html: item.comment,
+                      }}
+                    />
+                  </div>
+                </div>
+              ))}
           </div>
           <div className="flex items-center gap-2 mt-5">
             <img
-              src="https://deejayfarm.com/wp-content/uploads/2019/10/Profile-pic.jpg"
+              src="https://pbs.twimg.com/profile_images/981311875643195393/dS0t6BQ8_400x400.jpg"
               className="w-10 h-10 rounded-full object-cover"
             />
-            <form onSubmit={addComment} className="w-1/4">
+            <form onSubmit={addComment} className="w-full">
               <input
                 type="text"
                 placeholder="Add a comment..."
                 name="newcomment"
                 value={newcomment}
                 onChange={(e) => setNewcomment(e.target.value)}
-                className="w-full border-2 p-2"
+                className="w-full border-2 p-2 outline-none"
               />
               <button type="submit" className="hidden"></button>
             </form>
